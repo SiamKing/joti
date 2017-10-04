4d6d9cb1aaec66841479f70108ca5bad1e9bac00
SiamKing <thesiameseelectric@gmail.com>
2017-10-03 22:23:48 -0700

    smooth scrolling working. not using scrollTo

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,9 +16,11 @@ $(function() {
       if(scroll_start > offset.top) {
         $(".navbar-default").css({'background-color': '#fff', 'opacity': '1'});
         $(".nav-link").css('color', 'black');
+        $(".navbar-brand").css('color', 'black');
       } else {
         $('.navbar-default').css({'background-color': 'transparent'});
-        $(".nav-link").css({'color': 'white', 'background-color': 'transparent'});
+        $(".nav-link").css({'color': 'rgb(172, 248, 255)', 'background-color': 'transparent'});
+        $(".navbar-brand").css('color', 'white');
       }
     });
   }
@@ -35,19 +37,58 @@ $(function() {
   //   $('html, body').scrollTo(hash, {duration:1000});
   // });
 
-  $(".nav-link").click(function(e) {
-    var anchor = $(this).attr('href')
-    $('html, body').scrollTo(this.hash, 1800)
-    // remove classes from all
-    $(".nav-link").removeClass("active");
-    // add class to the one we clicked
-    $(this).addClass("active");
-  });
+  // $(".nav-link").click(function(e) {
+  //   var anchor = $(this).attr('href');
+  //   $('html, body').scrollTo(anchor, 1800)
+  //   // remove classes from all
+  //   $(".nav-link").removeClass("active");
+  //   // add class to the one we clicked
+  //   $(this).addClass("active");
+  // });
 //
-  $('.navbar-brand').click(function() {
-    console.log('navbar-brand')
-    $('html, body').scrollTo('home', {duration: 1800})
-  })
+
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      $('.nav-link').removeClass('active')
+      console.log(target)
+      $(this).addClass('active')
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1600, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  // $('.navbar-brand').click(function() {
+  //   console.log('navbar-brand')
+  //   $('body').scrollTo('#home', {duration: 1800})
+  // })
 });
 
 function onScroll(event){
