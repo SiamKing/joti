6bfd190de3dbaa1a648cfe9a0515d92b478e938d
SiamKing <thesiameseelectric@gmail.com>
2017-10-04 19:46:43 -0700

    added drop shadow to navbar on scroll. took circle out  behind chevron

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,29 +9,32 @@ $(function() {
   var whatWeDo = $('#what-we-do');
 
   var offset = whatWeDo.offset();
-  offset.top -= 10;
+  offset.top -= 100;
   if (whatWeDo.length){
     $(window).scroll(function(e) {
       scroll_start = $(this).scrollTop();
       // Check to see if position of current scroll is greater than the position of #what-we-do
       if(scroll_start > offset.top) {
         $(".navbar-default").css({'background-color': '#fff', 'opacity': '1'});
+        $(".navbar-default").addClass('nav-after-scroll');
         $(".nav-link").css('color', 'black');
-        $(".navbar-brand").css('color', 'black');
+        $(".navbar-brand").css('color', 'rgb(172, 248, 255)');
       } else {
         $('.navbar-default').css({'background-color': 'transparent'});
+        $(".navbar-default").removeClass('nav-after-scroll');
         $(".nav-link").css({'color': 'rgb(172, 248, 255)', 'background-color': 'transparent'});
         $(".navbar-brand").css('color', 'white');
+
       }
     });
   }
-  
+
   $('a[href^="#"]').on('click', function(event) {
     let target = $(this.hash);
     if (target.length) {
       event.preventDefault();
       $('html, body').stop().animate({
-          scrollTop: target.offset().top - 60
+          scrollTop: target.offset().top - 80
       }, 1000);
     }
   });
