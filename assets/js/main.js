5e98ff86679051514214ddb05331012bb4aa627a
SiamKing <thesiameseelectric@gmail.com>
2017-10-05 09:04:50 -0700

    added colors and circle around icons in what-we-do section

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,14 +29,58 @@ $(function() {
     });
   }
 
-  $('a[href^="#"]').on('click', function(event) {
-    let target = $(this.hash);
-    if (target.length) {
-      event.preventDefault();
-      $('html, body').stop().animate({
-          scrollTop: target.offset().top - 80
-      }, 1000);
-    }
-  });
+  $(document).on("scroll", onScroll);
 
+  // $('a[href^="#"]').on('click', function(event) {
+  //   let target = $(this.hash);
+  //   if (target.length) {
+  //
+  //     event.preventDefault();
+  //
+  //     $('html, body').stop().animate({
+  //         scrollTop: target.offset().top - 80
+  //     }, 1000);
+  //     $(".navbar-nav a").removeClass("active");
+  //     $(".navbar a").removeClass("active");
+  //     $(this).addClass("active");
+  //   }
+  // });
+
+  // smooth scroll when clicking navbar links and changes active class
+  $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 80
+        }, 800, 'swing', function () {
+            // window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 });
+
+// Changes current target's class to active on scroll
+function onScroll(event){
+  event.preventDefault();
+    var scrollPos = $(document).scrollTop();
+    $('.navbar-nav a').each(function (e) {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() +100 >= scrollPos) {
+            $('.navbar-nav a').removeClass('active');
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
