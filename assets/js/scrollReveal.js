820619ce8c307178854ede556a746fe18c13a925
SiamKing <thesiameseelectric@gmail.com>
2017-10-05 20:53:36 -0700

    added scrollReveal plugin and a few functions to reveal elements on scroll

---
+++ assets/js/scrollReveal.js
@@ -0,0 +1,12 @@
+$(function() {
+  window.sr = ScrollReveal();
+  sr.reveal('.joti-hero-text', { duration: 2000}, 50);
+  sr.reveal('.chevron');
+  // sr.reveal('h1');
+  // sr.reveal('.icon', { rotate: {x: 65}});
+  // sr.reveal('h3');
+  // sr.reveal('p');
+  // sr.reveal('.pride-section');
+  // sr.reveal('img');
+
+})
